c322cf83ab20940cc71631296ab3afae5d7d37b3
Abdelrahman Ahmed Sedky <168045943+3bdelra7manA7med@users.noreply.github.com>
2025-10-22 23:06:38 +0300

    Refactor landing page and update global styles
    Added new section and container classes to globals.css and updated button styles. Refactored the landing page to include Hero, IntroSection, Stats, Features, Contact, and Footer components for improved structure and modularity. Removed unused Google button and divider styles.

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,14 +1,25 @@
-'use client';
-import React from 'react'
+"use client";
+import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Hero/Hero";
+import IntroSection from "@/components/Intro/Intro";
+import Stats from "@/components/Stats/Stats";
+import Features from "@/components/Features/Features";
+import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 
 const LandingPage = () => {
-
   return (
     <>
       <Navbar />
+      <Hero />
+      <IntroSection />
+      <Stats />
+      <Features />
+      <Contact />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
